1c3f18bc931a6cee9e50cfbe8194fedaddd0a923
Dave Laffan <dave.laffan@outlook.com>
2019-11-13 18:08:04 +0000

    Make sidebar position fixed

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
     $('#sidebarToggle').on('click', function () {
-        $('#sidebar').toggleClass('toggle');
+        $('#sidebar, #content').toggleClass('display-switch');
     });
 
 });
