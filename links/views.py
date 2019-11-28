7eeb870168cadfe647b29e320b6bcc3fa2cfa051
Dave Laffan <dave.laffan@outlook.com>
2019-11-28 16:53:35 +0000

    Display bookmarks on screen, first pass
    Filters bookmarks by user, then splits into collection, then displays by
    collection.

--- links/views.py
+++ links/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
 
 from premium.utils import is_premium
+from .models import Bookmark, Collection
 
 
 # Create your views here.
 def links(request):
 
-    context = {}
+    collections = Collection.objects.filter(user__username=request.user)
+    collection_names = []
+    for collection in collections:
+        collection_names.append(collection.name)
+
+    coll_dict = {}
+
+    for i in range(len(collection_names)):
+        qs = Bookmark.objects.filter(
+            collection__name=collection_names[i]).order_by('position')
+        coll_dict[collection_names[i]] = qs
+
+    # for k, v in coll_dict.items():
+    #     print(k, v)
+
+    context = {'collections': coll_dict}
     context = is_premium(request.user, context)
 
     return render(request, 'links/links.html', context)
