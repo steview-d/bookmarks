e08b8fadd3bfa53950ac8ac3f1127ae8b8906871
Dave Laffan <dave.laffan@outlook.com>
2019-12-02 14:17:30 +0000

    Tidy code, rename vars

--- links/views.py
+++ links/views.py
@@ -9,17 +9,12 @@ from .models import Bookmark, Collection
 # Create your views here.
 def links(request):
 
-    bookmarks = Bookmark.objects.filter(user__username=request.user)  # noqa
-    collections = Collection.objects.filter(user__username=request.user).order_by('position')  # noqa
+    bookmarks = Bookmark.objects.filter(user__username=request.user)
     num_of_columns = 4  # noqa
-    collections_per_column = [] # needed?  # noqa
-    column_names = ['column_1', 'column_2', 'column_3', 'column_4', 'column_5']
-
     # eventually store this in relevant page object / model
     # just a 2d array in list form
     column_order_2 = [[1, 2, 3], [4, 5]]  # noqa
     column_order_3 = [[1, 2], [3, 4], [5]]  # noqa
-
     # using this one for now
     column_order_4 = [[1], [2], [3], [4, 5]]  # noqa
 
@@ -37,18 +32,16 @@ def links(request):
             )
             new_column_output[col][pos] = str(collection_name)
 
-    column_list = []
-
+    bm_data = []
     for x in range(num_of_columns):
         column = {}
         for j in (new_column_output[x]):
             qs = bookmarks.filter(collection__name=j).order_by('position')
             column[j] = qs
-        column_list.append(column)
+        bm_data.append(column)
 
     context = {"num_columns": num_of_columns,
-               "column_names": column_names,
-               "column_list": column_list}
+               "bm_data": bm_data}
     context = is_premium(request.user, context)
 
     return render(request, 'links/links.html', context)
