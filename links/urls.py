bfeb8a0fd1a2fe5abbc44ad179277cb045712859
Dave Laffan <dave.laffan@outlook.com>
2019-12-03 16:10:16 +0000

    On app start, loads the last viewed page
    Takes value from local session. If no value present, defaults to page at
    position 1.

--- links/urls.py
+++ links/urls.py
@@ -4,8 +4,9 @@ from .views import links
 from .utils import start_app, change_num_columns
 
 urlpatterns = [
-    path('<page>', links, name="links"),
     path('start_app', start_app, name="start_app"),
     path('change_num_columns/<page>/<num>', change_num_columns,
-         name="change_num_columns")
+         name="change_num_columns"),
+    path('<page>', links, name="links")
+
 ]
--- links/utils.py
+++ links/utils.py
@@ -4,10 +4,20 @@ from .models import Page
 
 
 def start_app(request):
-    # function to load app when coming from bm icon log or after first
-    # logging in will either point to page stored in session, or if no
-    # session default to page position value of 1
-    return redirect('links', page='home')
+    """
+    This function loads the app when coming from bm icon logo or after
+    logging in. It will either point to page the last page visited,
+    stored in session, or if no session value, it will load the page at
+    position 1.
+    """
+    try:
+        last_page = request.session['last_page']
+
+    except KeyError:
+        last_page = Page.objects.get(user=request.user, position=1)
+        request.session['last_page'] = last_page.name
+
+    return redirect('links', page='last_page')
 
 
 def change_num_columns(request, page, num):
