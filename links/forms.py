cf22c72b9ba5e273dbd4a304388f250d04edc5da
Dave Laffan <dave.laffan@outlook.com>
2019-12-09 10:28:44 +0000

    Refactor Page forms
    Both forms are similar and resue a lot of the same code. Create a new
    form 'PageForm' and subclass this to the 2 required forms to avoid
    repetition.

--- links/forms.py
+++ links/forms.py
@@ -3,7 +3,26 @@ from django import forms
 from .models import Page
 
 
-class AddNewPageForm(forms.ModelForm):
+class PageForm(forms.ModelForm):
+    class Meta:
+        model = Page
+        fields = ['name']
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('current_user')
+        super(PageForm, self).__init__(*args, **kwargs)
+
+    def clean_name(self):
+        """check the page name is unique to that user"""
+        name = self.cleaned_data.get('name').lower()
+
+        if Page.objects.filter(user=self.user).filter(name=name):
+            raise forms.ValidationError(
+                u'You already have a page with this name')
+        return name
+
+
+class AddNewPageForm(PageForm):
     num_of_columns = forms.IntegerField(
         initial=4, label="Columns"
     )
@@ -12,35 +31,8 @@ class AddNewPageForm(forms.ModelForm):
         model = Page
         fields = ['name', 'num_of_columns']
 
-    def __init__(self, *args, **kwargs):
-        self.user = kwargs.pop('current_user')
-        super(AddNewPageForm, self).__init__(*args, **kwargs)
 
-    def clean_name(self):
-        """check the page name is unique to that user"""
-        name = self.cleaned_data.get('name').lower()
-
-        if Page.objects.filter(user=self.user).filter(name=name):
-            raise forms.ValidationError(
-                u'You already have a page with this name')
-        return name
-
-
-class EditPageForm(forms.ModelForm):
-
+class EditPageForm(PageForm):
     class Meta:
         model = Page
         fields = ['name']
-
-    def __init__(self, *args, **kwargs):
-        self.user = kwargs.pop('current_user')
-        super(EditPageForm, self).__init__(*args, **kwargs)
-
-    def clean_name(self):
-        """check the page name is unique to that user"""
-        name = self.cleaned_data.get('name').lower()
-
-        if Page.objects.filter(user=self.user).filter(name=name):
-            raise forms.ValidationError(
-                u'You already have a page with this name')
-        return name
