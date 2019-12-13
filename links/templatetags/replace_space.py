5882c61edf1ff51618ac47bf98b6d0ec6c00b854
Dave Laffan <dave.laffan@outlook.com>
2019-12-13 08:40:36 +0000

    Add comments to custom filter, replace_space
    Also remove some template tags in base html that isn't required.

--- links/templatetags/replace_space.py
+++ links/templatetags/replace_space.py
@@ -4,4 +4,6 @@ register = template.Library()
 
 @register.filter
 def replace_space(value):
+    # custom filter to replace a space char with an underscore
+    # intended use is for generating HTML ID's from collection names
     return value.replace(' ', '_')
