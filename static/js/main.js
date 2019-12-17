d43df27ef1400ecb3ea80e96695b3424232c0761
Dave Laffan <dave.laffan@outlook.com>
2019-12-17 10:22:24 +0000

    Refactor main.js
    Collection sorting code put into a forEach loop to reduce repetition.
    Now posts correct data to server ready for processing.

--- static/js/main.js
+++ static/js/main.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
         $(this).submit();
     });
 
-    // Page Sorting
+    // Page Sorting ---------------------------------------------------------//
     // Use jQueryUI to sort page names then send new order
     // for processing using ajax
     $("#page-titles").sortable({
@@ -61,141 +61,65 @@ $(document).ready(function() {
         }
     });
 
-
-    // Collection Sorting
+    // Collection Sorting ---------------------------------------------------//
     // Use jQueryUI to sort collections into preferred columns and positions
     // and then send new order for processing using ajax
-    var new_collection_order = [[], [], [], [], []];
+
+    // var new_collection_order = [[], [], [], [], []];
+    var new_collection_order = [];
     function buildList(columnNum, columnData) {
-        new_collection_order[columnNum-1] = columnData;
+        // new_collection_order[columnNum-1] = columnData;
+        new_collection_order[columnNum] = columnData;
+        console.log(new_collection_order);
         return;
     }
 
-    function collection_order_ajax_call(postData) {
-        $.ajax({
-            type: "POST",
-            data: {
-                new_collection_order: postData,
-                csrfmiddlewaretoken: csrftoken
+    var column_list = [
+        "#column-1",
+        "#column-2",
+        "#column-3",
+        "#column-4",
+        "#column-5"
+    ];
+
+    column_list.forEach((key, idx) => {
+        $(key).sortable({
+            containment: "#collections-container",
+            cursor: "grabbing",
+            connectWith: [
+                "#column-1, #column-2",
+                "#column-3",
+                "#column-4",
+                "#column-5"
+            ],
+            deactivate: function() {
+                let data = $(this).sortable("serialize");
+                data = data.split("[]=.");
+                data.pop();
+
+                let newOrder = data.map(x => {
+                    return x.replace("&", "");
+                });
+                columnData = newOrder.join(",");
+                buildList(idx, columnData);
             },
-            url: "collection_sort",
-            success: function(data) {
-                if (data.success) {
-                    location.reload();
-                }
+            stop: function() {
+                let postData = JSON.stringify(new_collection_order);
+                // send the new collection orders to the server
+                $.ajax({
+                    type: "POST",
+                    data: {
+                        new_collection_order: postData,
+                        csrfmiddlewaretoken: csrftoken
+                    },
+                    url: "collection_sort",
+                    success: function(data) {
+                        if (data.success) {
+                            location.reload();
+                        }
+                    }
+                });
             }
         });
-    }
-
-    // Collection Sorting
-    $("#column-1").sortable({
-        containment: "#collections-container",
-        cursor: "grabbing",
-        connectWith: ['#column-1, #column-2', '#column-3', '#column-4', '#column-5'],
-        deactivate: function() {
-            let data = $(this).sortable("serialize");
-            data = data.split("[]=.");
-            data.pop();
-
-            let newOrder = data.map(i => {
-                return i.replace("&", "");
-            });
-            columnData = newOrder.join(",");
-            buildList(1, columnData);
-        },
-        stop: function() {
-            let postData = JSON.stringify(new_collection_order);
-            // send the new collection orders to the server
-            collection_order_ajax_call(postData);
-        }
-
-    });
-    $("#column-2").sortable({
-        containment: "#collections-container",
-        cursor: "grabbing",
-        connectWith: ['#column-1, #column-2', '#column-3', '#column-4', '#column-5'],
-        deactivate: function() {
-            let data = $(this).sortable("serialize");
-            data = data.split("[]=.");
-            data.pop();
-
-            let newOrder = data.map(i => {
-                return i.replace("&", "");
-            });
-            columnData = newOrder.join(",");
-            buildList(2, columnData);
-        },
-        stop: function() {
-            let postData = JSON.stringify(new_collection_order);
-            // send the new collection orders to the server
-            collection_order_ajax_call();
-        }
-
-    });
-    $("#column-3").sortable({
-        containment: "#collections-container",
-        cursor: "grabbing",
-        connectWith: ['#column-1, #column-2', '#column-3', '#column-4', '#column-5'],
-        deactivate: function() {
-            let data = $(this).sortable("serialize");
-            data = data.split("[]=.");
-            data.pop();
-
-            let newOrder = data.map(i => {
-                return i.replace("&", "");
-            });
-            columnData = newOrder.join(",");
-            buildList(3, columnData);
-        },
-        stop: function() {
-            let postData = JSON.stringify(new_collection_order);
-            // send the new collection orders to the server
-            collection_order_ajax_call();
-        }
-
-    });
-    $("#column-4").sortable({
-        containment: "#collections-container",
-        cursor: "grabbing",
-        connectWith: ['#column-1, #column-2', '#column-3', '#column-4', '#column-5'],
-        deactivate: function() {
-            let data = $(this).sortable("serialize");
-            data = data.split("[]=.");
-            data.pop();
-
-            let newOrder = data.map(i => {
-                return i.replace("&", "");
-            });
-            columnData = newOrder.join(",");
-            buildList(4, columnData);
-        },
-        stop: function() {
-            let postData = JSON.stringify(new_collection_order);
-            // send the new collection orders to the server
-            collection_order_ajax_call();
-        }
-
-    });
-    $("#column-5").sortable({
-        containment: "#collections-container",
-        cursor: "grabbing",
-        connectWith: ['#column-1, #column-2', '#column-3', '#column-4', '#column-5'],
-        deactivate: function() {
-            let data = $(this).sortable("serialize");
-            data = data.split("[]=.");
-            data.pop();
-
-            let newOrder = data.map(i => {
-                return i.replace("&", "");
-            });
-            columnData = newOrder.join(",");
-            buildList(5, columnData);
-        },
-        stop: function() {
-            let postData = JSON.stringify(new_collection_order);
-            // send the new collection orders to the server
-            collection_order_ajax_call();
-        }
-
     });
 });
