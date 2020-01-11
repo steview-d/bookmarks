0d690c153ee80ede50a58f60f7cd3ac5bf78edf3
Dave Laffan <dave.laffan@outlook.com>
2020-01-11 11:49:20 +0000

    Add search function and display results

--- search/views.py
+++ search/views.py
@@ -2,11 +2,21 @@ from django.shortcuts import render
 
 from premium.utils import is_premium
 
-from links.models import Page
+from links.models import Page, Bookmark
 
 
 # Create your views here.
 def search(request):
+    # Fetch form search query
+    q = request.GET.get('q')
+
+    # find bookmarks based on search query
+    search_results = Bookmark.objects.filter(
+        user=request.user, title__icontains=q
+    )
+
+    # get pages for sidebar
+    all_pages = Page.objects.filter(user=request.user).order_by('position')
 
     # set page value for default page choice for 'add bookmark' button
     try:
@@ -15,11 +25,11 @@ def search(request):
     except KeyError:
         page = Page.objects.get(user=request.user, position=1)
 
-    all_pages = Page.objects.filter(user=request.user).order_by('position')
-
     context = {
         "all_page_names": all_pages,
         "page": page,
+        "q": q,
+        "search_results": search_results
     }
     context = is_premium(request.user, context)
 
