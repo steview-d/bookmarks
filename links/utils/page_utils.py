64dbc983c3acedc24707722a2fce15d8a3f68a85
Dave Laffan <dave.laffan@outlook.com>
2020-01-13 14:21:58 +0000

    Add start of display mode options for bookmarks
    Currently, just displays (or not) certain elements dependant on the
    value of .display_mode.
    When styling site, will add options for user to choose a mode on a
    per collection basis.

--- links/utils/page_utils.py
+++ links/utils/page_utils.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404, redirect
 
 from django.contrib.auth.models import User
 
-from links.models import Page, Collection
+from links.models import Page
 
 
 def add_page(request, form_data):
