a9b82992252cc8947d662d6b19819c467a5c25ac
Dave Laffan <dave.laffan@outlook.com>
2020-01-23 16:54:09 +0000

    Add preview window for file upload

--- static/js/main.js
+++ static/js/main.js
@@ -347,4 +347,18 @@ $(document).ready(function() {
             }
         });
     });
+
+    // Image Upload
+    $('#id_icon').change(function () {
+        if (this.files && this.files[0]) {
+            var reader = new FileReader();
+
+
+            reader.onload = function (e) {
+                $('#img-preview').attr('src', e.target.result);
+            };
+
+            reader.readAsDataURL(this.files[0]);
+        }
+    });
 });
