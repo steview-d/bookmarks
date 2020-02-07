6bd5e8f56454123930133f901f11b67d4ed91191
Dave Laffan <dave.laffan@outlook.com>
2020-02-07 15:15:49 +0000

    Create ContactForm form object

---
+++ pages/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    """
+    Contact form for a non-registered/logged in user to
+    communicate with site admin
+    """
+
+    class Meta:
+        model = Contact
+        fields = ['name', 'emaail', 'message']
