93e6ca14a3e506e8c3315fe11b6182c0cb7c0919
Dave Laffan <dave.laffan@outlook.com>
2020-02-07 14:37:46 +0000

    Create Contact model for a contact form

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    """
+    ...
+    """
+    name = models.CharField(
+        max_length=30, null=False, blank=False
+    )
+    email = models.EmailField(
+        max_length=254, null=False, blank=False
+    )
+    message = models.CharField(
+            max_length=500, null=False, blank=False
+    )
+
+    class Meta:
+        def __str__(self):
+            return self.email
