8ba110f3a394a9e6b406ad7c0a9f7c868bf69634
Dave Laffan <dave.laffan@outlook.com>
2020-03-02 14:56:15 +0000

    Update validator with lower file size limit of 2mb

--- links/validators.py
+++ links/validators.py
@@ -8,7 +8,7 @@ def validate_icon_file_extension(value):
     Validator to ensure only specific filetypes can be used for
     bookmark icons
     """
-    f_ext = os.path.splitext(value.name)[1]
+    f_ext = os.path.splitext(value.name)[1].lower()
     allowed_types = ['.png', '.jpg', '.jpeg']
 
     if f_ext not in allowed_types:
@@ -20,11 +20,11 @@ def validate_icon_file_extension(value):
 def validate_icon_file_size(value):
     """
     Validator to ensure uploaded icon is below a certain size
-    Currently set to 3145728 bytes / 3MB
+    Currently set to 2097152 bytes / 2MB
     """
     f_size = value.size
 
-    if f_size > 3145728:
+    if f_size > 2097152:
         raise ValidationError(
-            u'Your file exceeds the maximum allowed size of 3MB - \
+            u'Your file exceeds the maximum allowed size of 2MB - \
                 please choose a different file.')
