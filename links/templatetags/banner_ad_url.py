fd6f3fc31a63080f5d57a882c4f8500cd4d793e9
Dave Laffan <dave.laffan@outlook.com>
2020-03-27 16:00:36 +0000

    Fix bug with ise500 on deployed site w/ debug off
    Bug only caused an error on deployed site with debug off, for reasons
    still unknown.
    Issue was with the template creating a url to the banner ad - url would
    create fine locally, but would error on remote deployment if debugging
    was off.
    Fixed by using a template filter to create entire path to image, rather
    than just generating image number and trying to slot it into template
    with rest of the path.

---
+++ links/templatetags/banner_ad_url.py
@@ -0,0 +1,22 @@
+from django import template
+
+from random import randint
+
+
+register = template.Library()
+
+
+@register.filter
+def create_ad_url(path, ext):
+    """
+    create a url to the ad image file
+
+    ad_num:  generates a random number that matches to one of
+             the available banner ads
+
+    adjust the second arg of randint to match the total
+    mumber of ads in rotation, ensuring ads files are named
+    ad1, ad2, upto ad(x)
+    """
+    ad_num = randint(1, 5)
+    return str(path) + str(ad_num) + str(ext)
