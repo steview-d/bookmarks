1636f42b0a25aa98fcb8792d4361a89989f6d367
Dave Laffan <dave.laffan@outlook.com>
2020-04-16 17:13:42 +0100

    Add 'views' tests for search app

--- search/tests.py
+++ search/tests.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.test.client import Client
+from django.urls import reverse
 
-# Create your tests here.
+from links.models import Page
+
+
+# --------------------- VIEWS ---------------------
+
+class TestSearchView(TestCase):
+    def setUp(self):
+        self.c = Client()
+        self.user = User.objects.create_user(
+            'test_user', 'a@b.com', 'test_password')
+        self.c.login(username='test_user', password='test_password')
+        Page.objects.create(name="test_page", position=1, )
+
+    def test_search_view(self):
+        response = self.c.get(reverse('search'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_search_loads_correct_template(self):
+        page = self.c.get(reverse('search'))
+        self.assertTemplateUsed(page, 'search/search_results.html')
