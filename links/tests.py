c6e49374771cf1f12fc82f94773a38fbab6e8a66
Dave Laffan <dave.laffan@outlook.com>
2020-04-16 10:37:57 +0100

    Upd tests for 'import' views & to check redirects

--- links/tests.py
+++ links/tests.py
@@ -36,6 +36,11 @@ def create_test_bookmark():
     )
 
 
+def create_test_url():
+    test_url = "http://google.com"
+    return test_url
+
+
 # --------------------- VIEWS ---------------------
 
 class TestMainAppView(TestCase):
@@ -54,6 +59,10 @@ class TestMainAppView(TestCase):
     def test_app_loads_correct_template(self):
         page = self.c.get(reverse('links', args=['test_page']))
         self.assertTemplateUsed(page, 'links/links.html')
+
+    def test_app_redirects_if_page_does_not_exist(self):
+        response = self.c.get(reverse('links', args=['non_existent_page']))
+        self.assertEqual(response.status_code, 302)
 
 
 class TestArrangeCollectionsView(TestCase):
@@ -74,6 +83,11 @@ class TestArrangeCollectionsView(TestCase):
         page = self.c.get(
             reverse('arrange_collections', args=['test_page']))
         self.assertTemplateUsed(page, 'links/arrange_collections.html')
+
+    def test_arrange_collections_redirects_if_page_does_not_exist(self):
+        response = self.c.get(
+            reverse('arrange_collections', args=['non_existent_page']))
+        self.assertEqual(response.status_code, 302)
 
 
 class TestEditBookmarkView(TestCase):
@@ -96,6 +110,16 @@ class TestEditBookmarkView(TestCase):
         page = self.c.get(reverse('edit_bookmark', args=['test_page', '1']))
         self.assertTemplateUsed(page, 'links/edit_bookmark.html')
 
+    def test_edit_bookmark_redirects_if_page_does_not_exist(self):
+        response = self.c.get(
+            reverse('edit_bookmark', args=['non_existent_page', '1']))
+        self.assertEqual(response.status_code, 302)
+
+    def test_edit_bookmark_redirects_if_bookmark_does_not_exist(self):
+        response = self.c.get(
+            reverse('edit_bookmark', args=['test_page', '2']))
+        self.assertEqual(response.status_code, 302)
+
 
 class TestAddBookmarkView(TestCase):
 
@@ -114,6 +138,11 @@ class TestAddBookmarkView(TestCase):
         page = self.c.get(reverse('add_bookmark', args=['test_page']))
         self.assertTemplateUsed(page, 'links/add_bookmark.html')
 
+    def test_add_bookmark_redirects_if_page_does_not_exist(self):
+        response = self.c.get(
+            reverse('add_bookmark', args=['non_existent_page']))
+        self.assertEqual(response.status_code, 302)
+
 
 class TestMoveBookmarkView(TestCase):
 
@@ -123,12 +152,27 @@ class TestMoveBookmarkView(TestCase):
             'test_user', 'a@b.com', 'test_password')
         self.c.login(username='test_user', password='test_password')
         create_test_page()
+        create_test_collection()
+        create_test_bookmark()
 
     def test_move_bookmark_view(self):
-        pass
+        response = self.c.get(
+            reverse('move_bookmark', args=['test_page', '1']))
+        self.assertEqual(response.status_code, 200)
 
     def test_move_bookmark_loads_correct_template(self):
-        pass
+        page = self.c.get(reverse('move_bookmark', args=['test_page', '1']))
+        self.assertTemplateUsed(page, 'links/move_bookmark.html')
+
+    def test_move_bookmark_redirects_if_page_does_not_exist(self):
+        response = self.c.get(
+            reverse('move_bookmark', args=['non_existent_page', '1']))
+        self.assertEqual(response.status_code, 302)
+
+    def test_move_bookmark_redirects_if_bookmark_does_not_exist(self):
+        response = self.c.get(
+            reverse('move_bookmark', args=['test_page', '2']))
+        self.assertEqual(response.status_code, 302)
 
 
 class TestImportUrlView(TestCase):
@@ -138,12 +182,23 @@ class TestImportUrlView(TestCase):
         self.user = User.objects.create_user(
             'test_user', 'a@b.com', 'test_password')
         self.c.login(username='test_user', password='test_password')
+        create_test_page()
+        create_test_collection()
+        create_test_bookmark()
 
     def test_import_url_view(self):
-        pass
+        test_url = create_test_url()
+        response = self.c.get(f'{reverse("import_url")}?url={test_url}')
+        self.assertEqual(response.status_code, 200)
 
     def test_import_url_loads_correct_template(self):
-        pass
+        test_url = create_test_url()
+        page = self.c.get(f'{reverse("import_url")}?url={test_url}')
+        self.assertTemplateUsed(page, 'links/import_url.html')
+
+    def test_view_redirects_if_no_url_to_import(self):
+        response = self.c.get(reverse('import_url'))
+        self.assertEqual(response.status_code, 302)
 
 
 class TestImportUrlSuccessView(TestCase):
@@ -156,7 +211,19 @@ class TestImportUrlSuccessView(TestCase):
         create_test_page()
 
     def test_import_url_success_view(self):
-        pass
+        session = self.c.session
+        session['imported_url'] = create_test_url()
+        session.save()
+        response = self.c.get(reverse('import_url_success'))
+        self.assertEqual(response.status_code, 200)
 
     def test_import_url_success_loads_correct_template(self):
-        pass
+        session = self.c.session
+        session['imported_url'] = create_test_url()
+        session.save()
+        page = self.c.get(reverse('import_url_success'))
+        self.assertTemplateUsed(page, 'links/import_url_success.html')
+
+    def test_import_url_success_redirects_if_no_url_in_session(self):
+        response = self.c.get(reverse('import_url_success'))
+        self.assertEqual(response.status_code, 302)
