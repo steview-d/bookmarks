6afd0d4d2907bf5baafcef84ae5eb54fd46a4f58
Dave Laffan <dave.laffan@outlook.com>
2020-04-16 14:44:58 +0100

    Move tests for 'links' app into own tests folder

---
+++ links/tests/setup.py
@@ -0,0 +1,36 @@
+from links.models import Page, Collection, Bookmark
+from links.utils.page_utils import build_empty_collection_order
+
+
+# --------------------- SETUP ---------------------
+
+def create_test_page():
+    Page.objects.create(
+        name="test_page",
+        position=1,
+        collection_order_2=build_empty_collection_order(2),
+        collection_order_3=build_empty_collection_order(3),
+        collection_order_4=build_empty_collection_order(4),
+        collection_order_5=build_empty_collection_order(5),
+        )
+
+
+def create_test_collection():
+    Collection.objects.create(
+        name="test_collection",
+        position=1,
+    )
+
+
+def create_test_bookmark():
+    Bookmark.objects.create(
+        url="http://www.google.com",
+        title="Google",
+        description="Search & Stuff",
+        position=1,
+    )
+
+
+def create_test_url():
+    test_url = "http://google.com"
+    return test_url
--- links/tests/test_views.py
+++ links/tests/test_views.py
@@ -3,42 +3,10 @@ from django.test import TestCase
 from django.test.client import Client
 from django.urls import reverse
 
-from .models import Page, Collection, Bookmark
-from .utils.page_utils import build_empty_collection_order
-
-
-# --------------------- SETUP ---------------------
-
-def create_test_page():
-    Page.objects.create(
-        name="test_page",
-        position=1,
-        collection_order_2=build_empty_collection_order(2),
-        collection_order_3=build_empty_collection_order(3),
-        collection_order_4=build_empty_collection_order(4),
-        collection_order_5=build_empty_collection_order(5),
-        )
-
-
-def create_test_collection():
-    Collection.objects.create(
-        name="test_collection",
-        position=1,
-    )
-
-
-def create_test_bookmark():
-    Bookmark.objects.create(
-        url="http://www.google.com",
-        title="Google",
-        description="Search & Stuff",
-        position=1,
-    )
-
-
-def create_test_url():
-    test_url = "http://google.com"
-    return test_url
+from .setup import (create_test_page,
+                    create_test_collection,
+                    create_test_bookmark,
+                    create_test_url)
 
 
 # --------------------- VIEWS ---------------------
